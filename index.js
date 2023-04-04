7eaf75bd4f1e9463fb5e055865e00c0b4ef3d318
Brandon Wallace <brandonwallace403@gmail.com>
2023-04-04 19:44:46 -0400

    adds completed lab

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+const element = document.createElement("div");
+document.getElementById('main').remove();
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.innerHTML = "YOUR-NAME is the champion"
